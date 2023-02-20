17d1fe36f94abd092161049e0549c150ede0a271
kerberus <gadielmaster1@gmail.com>
2023-02-20 16:52:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 JEU 
 
 
-etape 1: commencer dans une chambre royale avec une princesse (Ariane) atteint par le fleau qui crache du sang et l apparition du protagiste (Eron) qui dit "je dois absolument sauver la princesse" et au meme moment un erudit apparait dans la chambre et dit auprotagoniste  "la seule façon de sauver la princesse c'est de fabriquer la "LARME DIVINE"". (petite fenetre qui apparait et qui donne les astuces de fabrication) Fabrication : corne d'une licorne malefique, canine d'un vampire, larme d
+etape 1: commencer dans une chambre royale avec une princesse (Ariane) atteint par le fleau qui crache du sang et l apparition du protagiste (Eron) qui dit "je dois absolument sauver la princesse" et au meme moment un erudit apparait dans la chambre et dit au protagoniste  "la seule façon de sauver la princesse c'est de fabriquer la "LARME DIVINE"". (petite fenetre qui apparait et qui donne les astuces de fabrication) Fabrication : corne d'une licorne malefique, canine d'un vampire, larme d
 
 etape 2: dans la meme chambre se trouve un portail de teleportation (on va dire que c'est un pouvoir du protagoniste) qui lui permet de rentrer directement dans le labyrinthe (un fond avec des herbes) où il y a 3 monstres (des centaures) et un boss pour recuperer un des ingredients pour pouvoir creer la fameuse larme et le boss du 1er labyrinthe est une licorne AKA Black Licorne
 
 
 etape 3: apres avoir battue la licorne, sa corne drop (apparition d'une fenetre qui dit 1/3) puis un portail spawn pour le 2eme labyrinthe où il y aura cette fois 4 monstres (Chauves-souris) et un boss qui sera un vampire avec un fond tenebreux (et ne pas oublier la fenetre de statut 2/3 apres avoir battu et recuperer les dents de vampire ) 
 
-etape 4: encore un portail de teleportation (oui je sais ça fait bcp de portail de teleportation) qui menera vers l'antre de l'ange pleureur où il y aura (5 pretres et un ange comme boss) l'ange dropera une larme apres sa mort (et la fenetre apparaitra de nouveau etc)
+etape 4: encore un portail de teleportation (oui je sais ça fait bcp de portail de teleportation) qui menera vers l'autre de l'ange pleureur où il y aura (5 pretres et un ange comme boss) l'ange dropera une larme apres sa mort (et la fenetre apparaitra de nouveau etc)
 
 
 
