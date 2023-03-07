4632b398047a124a1eed27bff54a856a689d1dea
DaVeinOUT <113432060+DaVeinOUT@users.noreply.github.com>
2023-03-07 14:49:17 +0100

    Create constantes_collisions.py

---
+++ constantes/constantes_collisions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Les listes de constantes concernant les collisions.
+Groupes de collision etc.
+(A IMPORTER)
+
+"""
+import pygame as pg
+
+groupes = {
+    "tuile": pg.sprite.Group(),     # Liste des hitboxs pour les tuiles
+    "entite": pg.sprite.Group(),   # Liste des hitboxs pour les entiees
+    "joueur": pg.sprite.Group(),    # Liste des hitboxs pour les joueurs
+    "Monstre": pg.sprite.Group(),   # Liste des hitboxs pour les ennemis
+    "objet": pg.sprite.Group(),     # Liste des hitboxs pour les objets
+    "pnj": pg.sprite.Group(),       # Liste des hitboxs pour les personnages non joueurs
+    "tout": pg.sprite.Group()       # Liste des hitboxs pour tout
+}
+
+
