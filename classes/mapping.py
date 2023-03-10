03f026e8dbd92bd6e2ccfc241a590c7d2c911071
Tricus9 <125081764+Tricus9@users.noreply.github.com>
2023-03-10 14:19:55 -0300

    Update mapping.py

--- classes/mapping.py
+++ classes/mapping.py
@@ -1,8 +1,1 @@
-import pygame 
-def mapping():
-    # Création de la fenêtre
-    pygame.display.set_caption("")
-    screen = pygame.display.set_mode((960, 720))
 
-    # Importation du fond d'écran
-    background = pygame.image.load("").convert()
